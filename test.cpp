e8a75b698e56a2057850f88ca368f7bd0cc1cb84
jyang872 <jyang872@gatech.edu>
2026-02-24 00:30:00 -0500

    crude 3s central

--- test.cpp
+++ test.cpp
@@ -19,8 +19,8 @@ int main(int argc, char* argv[]) {
     CrudeModel model(BranchingStrategy::pseudo);
 
 
-    outsideAlgo CZalgo(&model,-23270,UBDSolver::IPOPT); // provide UBD for outer layer
-    //insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-23270,true,UBDSolver:: IPOPT); // provide UBD for outer layer
+    //outsideAlgo CZalgo(&model,-23270,UBDSolver::IPOPT); // provide UBD for outer layer
+    insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-23270,true,UBDSolver:: IPOPT); // provide UBD for outer layer
     //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
     //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
 
