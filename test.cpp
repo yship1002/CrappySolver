41a4299dbff80cc179f95323e23e5cb7d632ce1e
jyang872 <jyang872@gatech.edu>
2026-02-26 15:50:15 -0500

    try again with sigkill experiment

--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <csignal>
+#include <thread>
 #include "src/Algo.h"
 #include "src/BBNode.h"
 #include <example/ProcessModel.h>
@@ -20,41 +21,44 @@ int Tracker::strong_branching_lbd_calculation_count=0;
 std::vector<double> Tracker::strong_branching_lbd_calculation_time={};
 std::vector<double> Tracker::LBD_value_records={}; 
 std::string Tracker::file_name="test.json"; 
-bool terminate_flag = false;
-void handle_signal(int sig)
+static volatile std::sig_atomic_t terminate_flag = 0;
+void handle_signal(int)
 {
-    terminate_flag = true;
+    terminate_flag = 1;
 }
 int BBNode::node_counter=0;
 int main(int argc, char* argv[]) {
     std::signal(SIGTERM, handle_signal);
     Tracker::file_name=argv[1];
-    while (!terminate_flag) {
-        //ProcessMode: -1134.15(20s) -1134.15 (10s) -1126.4218270121305. (3s)
-        //Ex844:0.332724(3s) 0.325313529673937(10s) 0.325313529673937(20s)
-        //CrudeModel:-23273.75(5s)
-        ProcessModel model(BranchingStrategy::pseudo);
-
-        //outsideAlgo CZalgo(&model,-1126.4218270121305,UBDSolver::IPOPT); // provide UBD for outer layer
-        
-        insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-1126.4218270121305,solveFullmodel::yes,UBDSolver:: IPOPT); // provide UBD for outer layer
-        //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
-        //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
-
-        CZalgo.bestUBDforInfinity=true; // set this to true if you want to use the bestUBD for strong branching weight update when infeasible, set to false if you want to use 0 for weight update when infeasible
-        CZalgo.solve(1); // relgap=0.1% tolerance, abs=1
-
-        {
-            std::ofstream os(Tracker::file_name);
-            cereal::JSONOutputArchive oarchive(os);
-            Tracker::serialize(oarchive);
+    std::thread watcher([]{
+        while (!terminate_flag) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-    }
+        std::ofstream os(Tracker::file_name);
+        cereal::JSONOutputArchive oarchive(os);
+        Tracker::serialize(oarchive);
+    });
+
+    //ProcessMode: -1134.15(20s) -1134.15 (10s) -1126.4218270121305. (3s)
+    //Ex844:0.332724(3s) 0.325313529673937(10s) 0.325313529673937(20s)
+    //CrudeModel:-23273.75(5s)
+    ProcessModel model(BranchingStrategy::pseudo);
+
+    //outsideAlgo CZalgo(&model,-1126.4218270121305,UBDSolver::IPOPT); // provide UBD for outer layer
+    
+    insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-1126.4218270121305,solveFullmodel::yes,UBDSolver:: IPOPT); // provide UBD for outer layer
+    //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
+    //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
+
+    CZalgo.bestUBDforInfinity=true; // set this to true if you want to use the bestUBD for strong branching weight update when infeasible, set to false if you want to use 0 for weight update when infeasible
+    CZalgo.solve(1); // relgap=0.1% tolerance, abs=1
+
     {
         std::ofstream os(Tracker::file_name);
         cereal::JSONOutputArchive oarchive(os);
         Tracker::serialize(oarchive);
     }
+    watcher.join();
 
     return 0;
 }
