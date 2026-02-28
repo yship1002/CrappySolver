4956ebdbd1bdfcb5709b1cda81e29ec8bd9f2929
jyang872 <jyang872@gatech.edu>
2026-02-28 11:56:08 -0500

    ex844 3s crappy

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 #include "src/Algo.h"
 #include "src/BBNode.h"
-#include <example/ProcessModel.h>
+#include <example/Ex844Model.h>
 #include <cereal/types/vector.hpp>
 #include <cereal/types/utility.hpp>   // <-- THIS is the important one
 #include <cereal/types/string.hpp>
@@ -33,16 +33,16 @@ int main(int argc, char* argv[]) {
     //ProcessMode: -1134.15(20s) -1134.15 (10s) -1126.4218270121305. (3s)
     //Ex844:0.332724(3s) 0.325313529673937(10s) 0.325313529673937(20s)
     //CrudeModel:-23273.75(5s)
-    ProcessModel model(BranchingStrategy::pseudo);
+    Ex844Model model(BranchingStrategy::pseudo);
 
-    outsideAlgo CZalgo(&model,-1134.15,UBDSolver::IPOPT); // provide UBD for outer layer
+    outsideAlgo CZalgo(&model,0.332724*1000,UBDSolver::IPOPT); // provide UBD for outer layer
     
     //insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-23273.75,solveFullmodel::yes,UBDSolver:: IPOPT); // provide UBD for outer layer
     //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
     //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
 
     CZalgo.bestUBDforInfinity=true; // set this to true if you want to use the bestUBD for strong branching weight update when infeasible, set to false if you want to use 0 for weight update when infeasible
-    CZalgo.solve(1); // relgap=0.1% tolerance, abs=1
+    CZalgo.solve(0.3); // relgap=0.1% tolerance, abs=1
 
     {
         std::ofstream os(argv[1]);
