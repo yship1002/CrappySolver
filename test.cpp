d16d2beea8fac334c1612d5d241ab0f4626c1f7c
jyang872 <jyang872@gatech.edu>
2026-04-08 12:11:21 -0400

    24hr cz process 10s

--- test.cpp
+++ test.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     ProcessModel model(BranchingStrategy::pseudo);
 
 
-    outsideAlgo CZalgo(&model,-1126.4218270121305,UBDSolver::IPOPT); // provide UBD for outer layer
+    outsideAlgo CZalgo(&model,-1134.15,UBDSolver::IPOPT); // provide UBD for outer layer
     //insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-23270,true,UBDSolver:: GUROBI); // provide UBD for outer layer
     //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
     //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
