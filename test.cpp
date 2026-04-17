91471c4b98c9fa5c183d49ebde71a1d386119e9b
jyang872 <jyang872@gatech.edu>
2026-04-16 20:19:46 -0400

    ex844 30s

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 #include <thread>
 #include "src/Algo.h"
 #include "src/BBNode.h"
-#include <Crappy_Fuzzy_Problem_Library/Ex722.h>
+#include <Crappy_Fuzzy_Problem_Library/Ex844.h>
 #include <cereal/types/vector.hpp>
 #include <cereal/types/utility.hpp>   // <-- THIS is the important one
 #include <cereal/types/string.hpp>
@@ -40,14 +40,14 @@ int main(int argc, char* argv[]) {
     });
 
     //ProcessMode: -1134.15(20s) -1134.15 (10s) -1126.4218270121305. (3s)
-    //Ex844:0.332724(3s) 0.325313529673937(10s) 0.325313529673937(20s)
-    //CrudeModel: -19658.3 (121) -19204.1(12121) -18842.4(1212121212)
-    // Ex722:   -0.352759(10s)
-    Ex722Model model(BranchingStrategy::pseudo);
+    //Ex844:0.332724(3s) 0.325313529673937(10s) 0.325313529673937(20s,30s) 200
+    //CrudeModel: -19658.3 (121) -19204.1(12121) -18842.4(1212121212) 500
+    // Ex722:   -0.352759(10s) 200
+    Ex844Model model(BranchingStrategy::pseudo);
 
     //outsideAlgo CZalgo(&model,0.325313529673937*1000,UBDSolver::IPOPT); // provide UBD for outer layer
     
-    insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,-0.352759*1000,solveFullmodel::yes,UBDSolver:: IPOPT); // provide UBD for outer layer
+    insideAlgo CZalgo(&model,ScenarioNames::SCENARIO1,0.325313529673937*1000,solveFullmodel::yes,UBDSolver:: IPOPT); // provide UBD for outer layer
     //std::cout << "UBD is: "<<CZalgo.calculateUBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
     //std::cout << "LBD is: "<<CZalgo.calculateLBD(&(CZalgo.activeNodes[0]), 1)<<std::endl; // calculate LBD for root node before starting the algorithm, this is important for strong branching to have a good initial LBD for weight update when infeasible
 
